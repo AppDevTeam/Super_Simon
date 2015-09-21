f9c709d292b5ef805239fcef8e39fe9921db0d22
jkocina <jkocina.jr@gmail.com>
2015-09-21 12:21:03 -0500

    Update SimonView.java

--- src/SimonView.java
+++ src/SimonView.java
@@ -1,57 +1,21 @@
 import java.util.*;
+import java.awt.*;
+//import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
 
 /**
  *  Display for the Simon game
  *
+ * This class is going to accept values to indicate which colors to display
+ * 
  *  @author Zina Schroeder
  *  @version 1.0
  */
 public class SimonView {
 
-    // Instance Variables
-    private Simon simon;
-    private SimonView view;
-    private ScoreKeeper scoreKeeper;
-    private int round;
-
-    // Constructor
-    public SimonController() {
-
-        simon = new Simon();
-        view  = new SimonView();
-        scoreKeeper = new ScoreKeeper();
-        round = 0;
-    }
-
-    public runSimon() {
-
-        int turn = 0;
-        
-        // Prepare for the turn by choosing a color, etc. 
-        simon.prepareSimon(round);
-
-        // Display the colors
-        //Just a guess from Hilary!   
-            // Maybe this? We probably don't want to display the colors in this method 
-            // and calling the getColorPattern() method just returns the array of 
-            // colors without doing anything with it. - Zina
-        displayColorPattern(simon.getColorPattern());
-
-        // Expect user input
-
-
-        round++;
-
-    }
-
-    private void displayColorPattern(ArrayList pattern) {
-
-    }
-
-    private void listenForInput() {
-
-    }
-
+    public void displayScreen()
 
 
 }
