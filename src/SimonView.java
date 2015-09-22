35321d94b587f42e7a45753c5802ceafe2f8779d
jkocina <jkocina.jr@gmail.com>
2015-09-21 23:54:45 -0500

    SimonView.java
    We have a pane, label, and functionless reset button.

--- src/SimonView.java
+++ src/SimonView.java
@@ -1,9 +1,10 @@
+//import java.awt.Component;
+import java.awt.event.WindowEvent;
+import java.lang.Override;
 import java.util.*;
 import java.awt.*;
-//import java.awt.event.*;
-import javax.swing.*;
+import java.awt.event.*;
 import java.io.*;
-import javax.imageio.*;
 
 /**
  *  Display for the Simon game
@@ -13,19 +14,25 @@ import javax.imageio.*;
  *  @author Jacob Kocina
  *  @version 1.0
  */
-public class SimonView extends Frame {
+public class SimonView extends Frame implements WindowListener {
 
-    private JButton[][] board;
-    private JPanel panel;
+    private Panel panel;
+    private Label label;
+    private Button rsetButton;
 
     /**
      * This method begins the process of creating a display by giving variable access to the necessary graphics methods
      */
     public void displayScreen() {
 
-        panel = new JPanel(new GridLayout(1, 1));
-        board = new JButton[1][1];
+        // A panel that will hold displayed data
+        panel = new Panel();
 
+        // A centered label to be put into the panel
+        label = new Label("MITA-AppDev says: Follow These Colors", Label.CENTER);
+
+        //A button to reset things maybe
+        rsetButton = new Button("Reset");
         drawScreen();
 
     }
@@ -35,10 +42,23 @@ public class SimonView extends Frame {
      */
     private void drawScreen()  {
 
-        board[1][1] = new JButton();
-        board[1][1].setOpaque(true);
-        board[1][1].setBorderPainted(false);
-        board[1][1].setBackground(Color.black);
+        //sets layout and size of panel
+        setLayout(new BorderLayout(20,20));
+        setSize(500, 500);
+
+        //adds the label to the panel
+        add(label, BorderLayout.NORTH);
+
+        //adds the reset button to the panel
+        add(rsetButton, BorderLayout.SOUTH);
+
+        setTitle("MITA-AppDev Presents Simon Says");
+        //sets the panel and everything inside visible
+        setVisible(true);
+
+        //Sets up for x being clicked to System.exit(0)
+        addWindowListener(this);
+
     }
 
     /**
@@ -54,16 +74,41 @@ public class SimonView extends Frame {
 
         switch (color)  {
 
-            case 0: board[1][1].setBackground(Color.white);
+            case 0:
                     break;
-            case 1: board[1][1].setBackground(Color.red);
+            case 1:
                     break;
-            case 2: board[1][1].setBackground(Color.blue);
+            case 2:
                     break;
-            case 3: board[1][1].setBackground(Color.green);
+            case 3:
                     break;
-            case 4: board[1][1].setBackground(Color.yellow);
+            case 4:
                     break;
         }
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e)  {
+        System.exit(0);
+    }
+
+    //These things need to be here for the window listener
+    @Override
+    public void windowOpened(WindowEvent e) { }
+    @Override
+    public void windowClosed(WindowEvent e) { }
+    @Override
+    public void windowIconified(WindowEvent e) { }
+    @Override
+    public void windowDeiconified(WindowEvent e) { }
+    @Override
+    public void windowActivated(WindowEvent e) { }
+    @Override
+    public void windowDeactivated(WindowEvent e) { }
+
+    public static void main(String[] arguements) {
+
+        SimonView sv = new SimonView();
+        sv.displayScreen();
     }
 }
